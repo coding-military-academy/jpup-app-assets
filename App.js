15416c2b6705d2f8b6c5210cd791d1eb622bf380
gunheekim90 <gunhee21@gmail.com>
2024-11-16 18:10:09 +0900

    App.js

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { LogBox } from 'react-native';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
+  // LogBox.ignoreLogs(['2024']); 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <AppNavigator />
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
