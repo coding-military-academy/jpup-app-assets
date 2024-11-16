ca933ecbe4f10774dca10e6ffcf87555701eaace
gunheekim90 <gunhee21@gmail.com>
2024-11-16 16:05:20 +0900

    database.js

---
+++ src/services/database.js
@@ -0,0 +1,36 @@
+// src/services/database.js
+import { addDoc, collection, getDoc, getDocs } from 'firebase/firestore';
+import { db, auth } from './firebase';
+
+export const saveReading = async (cards) => {
+  const user = auth.currentUser;
+  if (user) {
+    await addDoc(collection(db, 'history'), {
+      userId: user.uid,
+      cards: cards.map((card) => ({
+        id: card.id,
+        name: card.name,
+        description: card.description,
+        frontImage: card.frontImage
+      })),
+      date: new Date().toISOString(),
+    });
+  }
+};
+
+export const getReadings = async () => {
+  const user = auth.currentUser;
+  if (user) {
+    const querySnapshot = await getDocs(collection(db, 'history'));
+    
+    // 가져온 데이터를 배열로 변환
+    const historyData = querySnapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data() // 문서의 데이터
+    }));
+
+    console.log(historyData); // 데이터를 확인하거나 상태로 저장
+    return historyData;
+  }
+  return [];
+};
