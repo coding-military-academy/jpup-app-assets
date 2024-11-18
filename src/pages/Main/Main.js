fe1c4a227fe10007dc1a677012ae785d061dc9db
gunheekim90 <gunhee21@gmail.com>
2024-11-18 17:04:28 +0900

    구글 애드몹 main.js

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,19 @@
 // src/pages/Main/Main.js
 import React from 'react';
-import { View } from 'react-native';
+import { Dimensions, Platform, View } from 'react-native';
 import { Button, Text } from 'react-native-paper';
 import { signOut } from '../../services/auth';
 import styles from './styles';
 
+import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+
+const windowWidth = Dimensions.get('window').width;
+// const unitId = Platform.OS == 'android' ? 'ca-app-pub-7866773177976164~1173874426' : 'ca-app-pub-7866773177976164/4037727980'
+//하단 계정은 테스트 아이디
+const unitId = TestIds.BANNER
+
 const Main = ({ navigation }) => {
+ 
   const handleLogout = async () => {
     try {
       await signOut();
@@ -16,7 +24,23 @@ const Main = ({ navigation }) => {
   };
 
   return (
+    <>
+    <View style={{
+      marginTop:50,
+      width:windowWidth,
+      alignItems:"center"
+    }}>
+      <BannerAd
+        unitId={unitId}
+        size={BannerAdSize.LARGE_BANNER}
+        onAdFailedToLoad={(err) =>{
+            console.log(err)
+        }}
+      />
+    </View>
+    
     <View style={styles.container}>
+      
       <Text style={styles.title}>타로 앱</Text>
       <Button
         mode="contained"
@@ -29,6 +53,8 @@ const Main = ({ navigation }) => {
         로그아웃
       </Button>
     </View>
+    </>
+    
   );
 };
 
