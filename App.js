702fdc9bcc9033f3d81af2935b77818b7aeb3649
gunheekim90 <gunhee21@gmail.com>
2024-11-18 17:02:37 +0900

    구글 애드몹 가로배너 App.js

--- App.js
+++ App.js
@@ -4,8 +4,14 @@ import { Provider as PaperProvider } from 'react-native-paper';
 import { LogBox } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 
+// Google Mobile Ads SDK 초기화
+import { useAdsInit } from './src/hook/ads';
+
 export default function App() {
   // LogBox.ignoreLogs(['2024']); 
+  setTimeout(() => {
+    useAdsInit()
+  }, 1000)
   return (
     <PaperProvider>
       <AppNavigator />
